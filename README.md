b3a23125d1f98df60b39733488ac7fcd51dcf39f
2dkot <baidinkn@ya.ru>
2016-10-16 22:51:30 +0300

    CORS support. Port configuration.

--- README.md
+++ README.md
@@ -1,7 +1,28 @@
 # web-server-kotlin
 It is implementation of AI-Contester web server in Kotlin using Spring
 
+## Build
+``` shell
+gradle build
+```
+
 ## Run
+``` shell
+cd build/libs
+java -jar web-server-0.1.0.jar
+```
+
+## Configuration
+### Port
+``` shell
+--port=1234
+```
+#### Example
+``` shell
+java -jar web-server-0.1.0.jar --port=1234
+```
+
+## Development run
 ``` shell
 gradle bootRun
 ```
---
+++ src/main/kotlin/server/SimpleCorsFilter.kt
@@ -0,0 +1,35 @@
+package server
+
+import org.springframework.core.Ordered
+import org.springframework.core.annotation.Order
+import org.springframework.stereotype.Component
+import java.io.IOException
+import javax.servlet.*
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@Component
+@Order(org.springframework.core.Ordered.HIGHEST_PRECEDENCE)
+class SimpleCorsFilter : Filter {
+    @Throws(IOException::class, ServletException::class)
+    override fun doFilter(req: ServletRequest, res: ServletResponse, chain: FilterChain) {
+        val response = res as HttpServletResponse
+        val request = req as HttpServletRequest
+        response.setHeader("Access-Control-Allow-Origin", "*")
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE")
+        response.setHeader("Access-Control-Max-Age", "3600")
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, content-type, origin, accept")
+
+        if ("OPTIONS".equals(request.method, ignoreCase = true)) {
+            response.status = HttpServletResponse.SC_OK
+        } else {
+            chain.doFilter(req, res)
+        }
+    }
+
+    override fun init(filterConfig: FilterConfig) {
+    }
+
+    override fun destroy() {
+    }
+}
